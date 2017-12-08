e780cfe04a73dd2eefd6e128869c3872869482e4
mightyBroccoli <nico@magicbroccoli.de>
2017-12-08 14:30:31 +0100

    * complete rehaul of the build/ precompress script
    * optipng with new parameters
    - removed the emtpy script.js file
    [BUILD]

--- build.sh
+++ build.sh
@@ -1,28 +1,59 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 ## Hugo Build script
 #
-DIRECTORY="/home/nico/Programming/Github/hugo-magicbroccoli.de"
 
+## variables
+Folders=("/home/nico/Programming/Github/hugo-magicbroccoli.de/public")
+ext="css|js|html|xml|svg|ico|eot|otf|ttf"
+
+## functions
+function compressResource {
+	gzip -c9 "$1" >"$1.gz"
+	touch -c --reference="$1" "$1.gz"
+	echo "Compressed: $1 > $1.gz"
+}
+
+## script
 # remove the old public directory
 echo "Removing old public directory"
-time cd $DIRECTORY && rm -rf ./public
+rm -rf "/home/nico/Programming/Github/hugo-magicbroccoli.de/public"
 
 # optimizing all png images with optipng
 echo "Optimizing static/*.png"
-time for x in `find $DIRECTORY/static/ -type f -name '*.png'`; do
-	optipng -o5 -silent ${x};
-done
+#find $DIRECTORY/static/ -type f -name '*.png' -print0 | xargs optipng -nc -nb -o7 -silent
 
 # builging latest site
-cd $DIRECTORY && hugo
+hugo --source "/home/nico/Programming/Github/hugo-magicbroccoli.de/"
 
-# precompressing html css and js files
-echo "precompressing *.html *.css and *.js files"
-time for x in `find $DIRECTORY/public -type f -name '*.html' -o -name '*.css' -o -name '*.js'`; do
-     gzip --best --keep --force ${x};
+# precompiled list of folders
+for appDir in "${Folders[@]}"
+do
+	# fetch all existing gzipped CSS/JavaScript/webfont files and remove files that do not have a base uncompressed file
+	find "$appDir" -type f -regextype posix-extended -iregex ".*\\.(${ext})\\.gz$" -print0 | while read -d '' compressFile
+	do
+		if [[ ! -f ${compressFile%.gz} ]]; then
+			# remove orphan gzipped file
+			rm "$compressFile"
+			echo "Removed: $compressFile"
+		fi
+	done
+
+	# fetch all source CSS/JS/webfont files - excluding *.src.* variants (pre-minified CSS/JavaScript)
+	# gzip each file and give timestamp identical to that of the uncompressed source file
+	find "$appDir" -type f -regextype posix-extended \( -iregex ".*\\.(${ext})$" \) \( ! -name "*.src.css" -and ! -name "*.src.js" \) -print0 | while read -d '' sourceFile
+	do
+		if [[ -f "$sourceFile.gz" ]]; then
+			# only re-gzip if source file is different in timestamp to the existing gzip file
+			if [[ ($sourceFile -nt "$sourceFile.gz") || ($sourceFile -ot "$sourceFile.gz") ]]; then
+				compressResource "$sourceFile"
+			fi
+		else
+			compressResource "$sourceFile"
+		fi
+	done
 done
 
 # clearing variables
-unset DIRECTORY
+unset Folders ext
 exit 0
