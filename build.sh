f7a7c581c4f11139e0e229a8af977a51b18c2f3f
mightyBroccoli <nico@magicbroccoli.de>
2018-04-10 18:30:20 +0200

    * moved rss.xml to the correct place
    * updated config.toml
    * updated build script
    * optimized images
    [BUILD]

--- build.sh
+++ build.sh
@@ -3,15 +3,7 @@
 ## Hugo Build script
 #
 ## variables
-Folders=("/home/nico/Programming/Github/hugo-magicbroccoli.de")
-ext="css|js|html|xml|svg|ico|eot|otf|ttf"
-
-## functions
-function compressResource {
-	gzip -c9 "$1" >"$1.gz"
-	touch -c --reference="$1" "$1.gz"
-	echo "Compressed: $1 > $1.gz"
-}
+Folders=( /home/nico/Programming/Github/hugo-magicbroccoli.de )
 
 ## script
 # remove the old public directory
@@ -20,42 +12,13 @@ rm -rf "/home/nico/Programming/Github/hugo-magicbroccoli.de/public"
 
 # optimizing all png images with optipng
 echo "Optimizing static/*.png"
-for appDir in "${Folders[@]}"
-do
-	find "$appdir"/static/ -type f -regextype posix-extended -iregex ".*\\.png$" | xargs optipng -nc -nb -o7 -silent
+for i in "${Folders[@]}"; do
+	find "$i/static/" -type f -regextype posix-extended -iregex ".*\\.png$" | xargs optipng -nc -nb -o7 -silent
 done
 
 # builging latest site
 hugo --source "/home/nico/Programming/Github/hugo-magicbroccoli.de/"
 
-# precompiled list of folders
-for appDir in "${Folders[@]}/public/"
-do
-	# fetch all existing gzipped CSS/JavaScript/webfont files and remove files that do not have a base uncompressed file
-	find "$appDir" -type f -regextype posix-extended -iregex ".*\\.(${ext})\\.gz$" -print0 | while read -d '' compressFile
-	do
-		if [[ ! -f ${compressFile%.gz} ]]; then
-			# remove orphan gzipped file
-			rm "$compressFile"
-			echo "Removed: $compressFile"
-		fi
-	done
-
-	# fetch all source CSS/JS/webfont files - excluding *.src.* variants (pre-minified CSS/JavaScript)
-	# gzip each file and give timestamp identical to that of the uncompressed source file
-	find "$appDir" -type f -regextype posix-extended \( -iregex ".*\\.(${ext})$" \) \( ! -name "*.src.css" -and ! -name "*.src.js" \) -print0 | while read -d '' sourceFile
-	do
-		if [[ -f "$sourceFile.gz" ]]; then
-			# only re-gzip if source file is different in timestamp to the existing gzip file
-			if [[ ($sourceFile -nt "$sourceFile.gz") || ($sourceFile -ot "$sourceFile.gz") ]]; then
-				compressResource "$sourceFile"
-			fi
-		else
-			compressResource "$sourceFile"
-		fi
-	done
-done
-
 # clearing variables
-unset Folders ext
+unset Folders
 exit 0
